8b7a1e5eb8ee31f02b5cc7191b0ee2792093db96
omanfathurrahman <134471146+Rizal3004@users.noreply.github.com>
2024-08-14 15:42:27 +0700

    ganti base url ke url server

--- src/ofetch.ts
+++ src/ofetch.ts
@@ -1,6 +1,6 @@
 import { ofetch } from 'ofetch'
 
-export const baseURL = 'http://localhost:8787'
+export const baseURL = 'https://puskesmas-tamblong-server.puskesmas-tamblong-api.workers.dev/'
 const apiFetch = ofetch.create({
   baseURL,
 })
