c8f506c24a2c08b7e1596cb17043b5f075897630
Rizal3004 <134471146+Rizal3004@users.noreply.github.com>
2024-09-15 20:48:40 +0700

    update

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -94,147 +94,149 @@ onMounted(() => {
     </RouterLink>
     <h1 class="text-2xl font-semibold">Profil</h1>
     <table v-if="profile" class="w-8/12 [&>*>*]:border [&>*>*]:px-2 [&>*>*]:text-start">
-      <!-- <tr>
-        <th>ID</th>
-        <td>{{ profile.id }}</td>
-      </tr> -->
-      <tr>
-        <th>Nama</th>
-        <td>{{ profile.name }}</td>
-      </tr>
-      <tr>
-        <th>NIK</th>
-        <td>{{ profile.nik }}</td>
-      </tr>
-      <tr>
-        <th>Email</th>
-        <td>
-          <div v-if="!isEditEmail" class="flex justify-between">
-            <p>
-              {{ profile.email ?? 'Belum diatur' }}
-            </p>
-            <button @click="isEditEmail = true">
-              <SolarPen2BoldDuotone class="text-orange-400" />
-            </button>
-          </div>
-          <div v-else class="flex justify-between p-1">
-            <input
-              ref="emailEl"
-              v-model="emailText"
-              type="email"
-              class="w-full border p-1"
-              placeholder="Masukkan email baru anda"
-            >
-            <div class="flex gap-3">
-              <button @click="isEditEmail = false">
-                <SolarCloseSquareLineDuotone class="text-red-400" />
-              </button>
-              <button @click="handleChangeEmail">
-                <SolarCheckSquareLineDuotone class="text-green-400" />
+      <tbody>
+        <!-- <tr>
+          <th>ID</th>
+          <td>{{ profile.id }}</td>
+        </tr> -->
+        <tr>
+          <th>Nama</th>
+          <td>{{ profile.name }}</td>
+        </tr>
+        <tr>
+          <th>NIK</th>
+          <td>{{ profile.nik }}</td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td>
+            <div v-if="!isEditEmail" class="flex justify-between">
+              <p>
+                {{ profile.email ?? 'Belum diatur' }}
+              </p>
+              <button @click="isEditEmail = true">
+                <SolarPen2BoldDuotone class="text-orange-400" />
               </button>
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <th>Alamat</th>
-        <td>
-          <div v-if="!isEditAddress" class="flex justify-between">
-            <p>
-              {{ profile.address ?? 'Belum diatur' }}
-            </p>
-            <button @click="isEditAddress = true">
-              <SolarPen2BoldDuotone class="text-orange-400" />
-            </button>
-          </div>
-          <div v-else class="flex justify-between p-1">
-            <input
-              ref="addressEl"
-              v-model="addressText"
-              type="text"
-              class="w-full border p-1"
-              placeholder="Masukkan alamat baru anda"
-            >
-            <div class="flex gap-3">
-              <button @click="isEditAddress = false">
-                <SolarCloseSquareLineDuotone class="text-red-400" />
-              </button>
-              <button @click="handleChangeAddress">
-                <SolarCheckSquareLineDuotone class="text-green-400" />
+            <div v-else class="flex justify-between p-1">
+              <input
+                ref="emailEl"
+                v-model="emailText"
+                type="email"
+                class="w-full border p-1"
+                placeholder="Masukkan email baru anda"
+              >
+              <div class="flex gap-3">
+                <button @click="isEditEmail = false">
+                  <SolarCloseSquareLineDuotone class="text-red-400" />
+                </button>
+                <button @click="handleChangeEmail">
+                  <SolarCheckSquareLineDuotone class="text-green-400" />
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th>Alamat</th>
+          <td>
+            <div v-if="!isEditAddress" class="flex justify-between">
+              <p>
+                {{ profile.address ?? 'Belum diatur' }}
+              </p>
+              <button @click="isEditAddress = true">
+                <SolarPen2BoldDuotone class="text-orange-400" />
               </button>
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <th>No Telepon</th>
-        <td>
-          <div v-if="!isEditPhone" class="flex justify-between">
-            <p>
-              {{ profile.phone ?? 'Belum diatur' }}
-            </p>
-            <button @click="isEditPhone = true">
-              <SolarPen2BoldDuotone class="text-orange-400" />
-            </button>
-          </div>
-          <div v-else class="flex justify-between p-1">
-            <input
-              ref="phoneEl"
-              v-model="phoneText"
-              type="text"
-              class="w-full border p-1"
-              placeholder="Masukkan nomor baru anda"
-              @keypress="validateInputNumber"
-            >
-            <div class="flex gap-3">
-              <button @click="isEditPhone = false">
-                <SolarCloseSquareLineDuotone class="text-red-400" />
-              </button>
-              <button @click="handleChangePhone">
-                <SolarCheckSquareLineDuotone class="text-green-400" />
+            <div v-else class="flex justify-between p-1">
+              <input
+                ref="addressEl"
+                v-model="addressText"
+                type="text"
+                class="w-full border p-1"
+                placeholder="Masukkan alamat baru anda"
+              >
+              <div class="flex gap-3">
+                <button @click="isEditAddress = false">
+                  <SolarCloseSquareLineDuotone class="text-red-400" />
+                </button>
+                <button @click="handleChangeAddress">
+                  <SolarCheckSquareLineDuotone class="text-green-400" />
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th>No Telepon</th>
+          <td>
+            <div v-if="!isEditPhone" class="flex justify-between">
+              <p>
+                {{ profile.phone ?? 'Belum diatur' }}
+              </p>
+              <button @click="isEditPhone = true">
+                <SolarPen2BoldDuotone class="text-orange-400" />
               </button>
             </div>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <th>Tanggal Lahir</th>
-        <td>
-          <p>
-            {{ profile.birthdate ?? 'Belum diatur' }}
-          </p>
-        </td>
-      </tr>
-      <tr>
-        <th>Password</th>
-        <td>
-          <div v-if="!isEditPassword" class="flex justify-between">
+            <div v-else class="flex justify-between p-1">
+              <input
+                ref="phoneEl"
+                v-model="phoneText"
+                type="text"
+                class="w-full border p-1"
+                placeholder="Masukkan nomor baru anda"
+                @keypress="validateInputNumber"
+              >
+              <div class="flex gap-3">
+                <button @click="isEditPhone = false">
+                  <SolarCloseSquareLineDuotone class="text-red-400" />
+                </button>
+                <button @click="handleChangePhone">
+                  <SolarCheckSquareLineDuotone class="text-green-400" />
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th>Tanggal Lahir</th>
+          <td>
             <p>
-              *********
+              {{ profile.birthdate ?? 'Belum diatur' }}
             </p>
-            <button @click="isEditPassword = true">
-              <SolarPen2BoldDuotone class="text-orange-400" />
-            </button>
-          </div>
-          <div v-else class="flex justify-between p-1">
-            <input
-              ref="passwordEl"
-              v-model="passwordText"
-              type="text"
-              class="w-full border p-1"
-              placeholder="Masukkan password baru anda"
-            >
-            <div class="flex gap-3">
-              <button @click="isEditPassword = false">
-                <SolarCloseSquareLineDuotone class="text-red-400" />
-              </button>
-              <button @click="handleChangePassword">
-                <SolarCheckSquareLineDuotone class="text-green-400" />
+          </td>
+        </tr>
+        <tr>
+          <th>Password</th>
+          <td>
+            <div v-if="!isEditPassword" class="flex justify-between">
+              <p>
+                *********
+              </p>
+              <button @click="isEditPassword = true">
+                <SolarPen2BoldDuotone class="text-orange-400" />
               </button>
             </div>
-          </div>
-        </td>
-      </tr>
+            <div v-else class="flex justify-between p-1">
+              <input
+                ref="passwordEl"
+                v-model="passwordText"
+                type="text"
+                class="w-full border p-1"
+                placeholder="Masukkan password baru anda"
+              >
+              <div class="flex gap-3">
+                <button @click="isEditPassword = false">
+                  <SolarCloseSquareLineDuotone class="text-red-400" />
+                </button>
+                <button @click="handleChangePassword">
+                  <SolarCheckSquareLineDuotone class="text-green-400" />
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
     <div class="flex gap-5">
       <button class="rounded-md bg-red-500 px-3 py-1 text-white" @click="handleLogout">Logout</button>
